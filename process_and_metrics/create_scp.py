77840949c8d86769c2517dc8f249f9339837d0b0
proudpie <134203137+proudpie@users.noreply.github.com>
2025-10-09 17:09:14 +0800

    create_scp.py

--- process_and_metrics/create_scp.py
+++ process_and_metrics/create_scp.py
@@ -1,20 +1,28 @@
 import os
 
-train_mix_scp = 'tr_mix.scp'
-train_s1_scp = 'tr_s1.scp'
-train_s2_scp = 'tr_s2.scp'
+train_mix_scp = 'tr_mix_xx.scp'
+train_s1_scp = 'tr_s1_xx.scp'
+train_s2_scp = 'tr_s2_xx.scp'
 
-test_mix_scp = 'tt_mix.scp'
-test_s1_scp = 'tt_s1.scp'
-test_s2_scp = 'tt_s2.scp'
+test_mix_scp = 'tt_mix_xx.scp'
+test_s1_scp = 'tt_s1_xx.scp'
+test_s2_scp = 'tt_s2_xx.scp'
 
-train_mix = '/home/likai/likai/Dataset/wsj0-mix/2speakers/wav8k/min/tr/mix'
-train_s1 = '/home/likai/likai/Dataset/wsj0-mix/2speakers/wav8k/min/tr/s1'
-train_s2 = '/home/likai/likai/Dataset/wsj0-mix/2speakers/wav8k/min/tr/s2'
+cv_mix_scp = 'cv_mix_xx.scp'
+cv_s1_scp = 'cv_s1_xx.scp'
+cv_s2_scp = 'cv_s2_xx.scp'
 
-test_mix = '/home/likai/likai/Dataset/wsj0-mix/2speakers/wav8k/min/tt/mix'
-test_s1 = '/home/likai/likai/Dataset/wsj0-mix/2speakers/wav8k/min/tt/s1'
-test_s2 = '/home/likai/likai/Dataset/wsj0-mix/2speakers/wav8k/min/tt/s2'
+train_mix = ''
+train_s1 = ''
+train_s2 = ''
+
+test_mix = ''
+test_s1 = ''
+test_s2 = ''
+
+cv_mix = ''
+cv_s1 = ''
+cv_s2 = ''
 
 tr_mix = open(train_mix_scp,'w')
 for root, dirs, files in os.walk(train_mix):
@@ -40,7 +48,6 @@ for root, dirs, files in os.walk(train_s2):
         tr_s2.write('\n')
 
 
-
 tt_mix = open(test_mix_scp,'w')
 for root, dirs, files in os.walk(test_mix):
     files.sort()
@@ -64,15 +71,8 @@ for root, dirs, files in os.walk(test_s2):
         tt_s2.write(file+" "+root+'/'+file)
         tt_s2.write('\n')
 
-cv_mix_scp = 'cv_mix.scp'
-cv_s1_scp = 'cv_s1.scp'
-cv_s2_scp = 'cv_s2.scp'
 
-cv_mix = '/home/likai/likai/Dataset/wsj0-mix/2speakers/wav8k/min/cv/mix'
-cv_s1 = '/home/likai/likai/Dataset/wsj0-mix/2speakers/wav8k/min/cv/s1'
-cv_s2 = '/home/likai/likai/Dataset/wsj0-mix/2speakers/wav8k/min/cv/s2'
-
-cv_mix_file = open(cv_mix_scp,'w')
+cv_mix = open(cv_mix_scp,'w')
 for root, dirs, files in os.walk(cv_mix):
     files.sort()
     for file in files:
@@ -80,7 +80,7 @@ for root, dirs, files in os.walk(cv_mix):
         cv_mix_file.write('\n')
 
 
-cv_s1_file = open(cv_s1_scp,'w')
+cv_s1 = open(cv_s1_scp,'w')
 for root, dirs, files in os.walk(cv_s1):
     files.sort()
     for file in files:
@@ -88,7 +88,7 @@ for root, dirs, files in os.walk(cv_s1):
         cv_s1_file.write('\n')
 
 
-cv_s2_file = open(cv_s2_scp,'w')
+cv_s2 = open(cv_s2_scp,'w')
 for root, dirs, files in os.walk(cv_s2):
     files.sort()
     for file in files:
